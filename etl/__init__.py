262f7be94309b05f607c225e0652bb953c0292ef
brokendeveloper <fluccas09@gmail.com>
2026-03-20 22:41:00 -0300

    chore: cria estrutura de diretórios (db/ e pacote etl/)

---
+++ etl/__init__.py
@@ -0,0 +1,8 @@
+"""
+Pacote ETL para extração, transformação e carga de dados de universidades.
+
+Módulos:
+    extract: Extração de dados da API Hipolabs.
+    transform: Limpeza e transformação dos dados brutos.
+    load: Carregamento dos dados no banco SQLite.
+"""
